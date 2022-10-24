067a57665452eb3767bf1cd2c1b1a5c4eb3b52aa
Dakota Newman <dakota.newman@yahoo.com>
2022-10-24 15:37:28 -0400

    added to file to complete requested changes

---
+++ main.js
@@ -0,0 +1,25 @@
+let para = document.querySelector('p')
+para.style.color = 'lightblue'
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let header = document.querySelector('h1')
+header.style.fontSize = '48px'
+
+let item_13 = document.querySelector('#item-13')
+item_13.style.opacity = 0
+
+let item_3 = document.querySelector('#item-3')
+item_3.innerText = 'Hi!'
+
+let img = document.querySelector('img')
+img.src = 'https://www.tastingtable.com/img/gallery/the-10-best-drinks-to-mix-with-beer/l-intro-1662067134.jpg'
+img.style.height = '300px'
+
+let myImg = document.querySelector('#myImage')
+myImg.style.height = '300px'
+
+let item_16 = document.querySelector('#item-16')
+
+item_16.className = 'item'
+item_16.innerText = "Won't get fooled again."
+
